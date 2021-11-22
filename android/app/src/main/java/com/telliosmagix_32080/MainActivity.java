12b496633671cdf52cd1e8aa0fe0fcc5452608a1
Crowdbotics <team@crowdbotics.com>
2021-11-22 07:09:35 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/telliosmagix_32080/MainActivity.java
@@ -0,0 +1,15 @@
+package com.telliosmagix_32080;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "telliosmagix_32080";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "telliosmagix_32080",
+  url: "https://telliosmagix_32080.botics.co",
+  api: "https://telliosmagix_32080.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
